6231a251cad85b532f7706e7fe70e5225679cb9f
edo <eabagdasarian@edu.hse.ru>
2024-12-07 21:44:56 +0300

    crons and requirement update

--- requests and update/regular script.py
+++ requests and update/regular script.py
@@ -1,2 +1,15 @@
-if __name__ == 'main':
-    pass
+import schedule
+import time
+from update_db import update
+
+
+def job():
+    """Задача, которая будет выполняться каждые 5 минут"""
+    update()
+
+
+schedule.every(5).minutes.do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
